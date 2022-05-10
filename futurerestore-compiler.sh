ef0aca18931adf9fde70d04e537cef3a033ad232
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-05-10 21:49:18 +0700

    Update futurerestore-compiler.sh

--- futurerestore-compiler.sh
+++ futurerestore-compiler.sh
@@ -40,7 +40,7 @@ if [[ $@ != *"--without-dependencies"* ]]; then
 	done
 	
 	# Gain sudo permission
-	sudo -s
+	sudo -i
 
 	# Clone dependencies
 	DEPENDENCIES=("https://github.com/libimobiledevice/libplist" "https://github.com/libimobiledevice/libusbmuxd" "https://github.com/libimobiledevice/libirecovery" "https://github.com/libimobiledevice/libimobiledevice-glue" "https://github.com/nyuszika7h/xpwn" "https://github.com/tihmstar/libgeneral" "https://github.com/tihmstar/libfragmentzip" "https://github.com/tihmstar/libinsn" "https://github.com/tihmstar/img4tool" "https://github.com/Cryptiiiic/liboffsetfinder64" "https://github.com/Cryptiiiic/libipatcher")
@@ -78,7 +78,7 @@ if [[ $@ != *"--without-dependencies"* ]]; then
 fi
 
 # Clone futurerestore
-sudo -s
+sudo -i
 rm -rf futurerestore &> /dev/null
 if [ $choice = "1" ]; then
 	sudo git clone -b test --recursive https://github.com/futurerestore/futurerestore
